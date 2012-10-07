7e4d2ebb7a17878724e914f9f18f6023b43381d4
Craig Ulmer <craig@craigulmer.com>
2012-10-06 23:09:36 -0700

    Updated to F17, had to remove emacs fonts and key things

---
+++ bash/cdu_bash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export GOROOT=/opt/go
+export GOARCH=amd64
+export GOBIN=$GOROOT/bin
+export GOPATH=$HOME/projects/go
+
+export PATH=$PATH:$GOBIN
--- redo_links.sh
+++ redo_links.sh
@@ -3,4 +3,14 @@
 
 CONF_HOME=~/projects/linux_confs
 
+
 ln -s $CONF_HOME/emacs/emacs ~/.emacs
+
+
+# Add our bash junk into the rc
+num=`grep cdu_bash $HOME/.bashrc | wc -l`
+echo $num
+if [ "$num" -eq "0" ]; then
+  echo ". $HOME/projects/linux_confs/bash/cdu_bash.sh" >> $HOME/.bashrc
+fi
+
