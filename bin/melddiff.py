f040bef5205b9400760d3aff2c9088d7dc71838f
Craig Ulmer <cdulmer@sandia.gov>
2014-09-23 21:35:27 -0700

    NEW: Added script to pipe diffs into meld

---
+++ bin/melddiff.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#
+# Simple script to allow git to pipe difference to meld
+# from some other website (not sure where?)
+#
+# Enable via:
+#   git config --global diff.external \
+#               /home/cdulmer/projects/linux_confs/scripts/melddiff.py
+# 
+import sys
+import os
+
+os.system('meld "%s" "%s"' % (sys.argv[2], sys.argv[5]))
+
